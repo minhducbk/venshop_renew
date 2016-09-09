5b3cfe1fdfac2211da755e84645fc1d8cb6a8438
Minh Duc Bui <ducbk95@gmail.com>
2016-09-09 14:15:35 +0700

    Finish basic batch process

--- app/workers/mail_worker.rb
+++ app/workers/mail_worker.rb
@@ -1,6 +1,5 @@
 class MailWorker < ApplicationController
   include Sidekiq::Worker
-  sidekiq_options retry: false
 
   def perform(email)
     UserMailer.finish_checkout(email).deliver_now
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,3 @@
-every 10.minutes, RAILS_ENV: :development do
-  rake "lib:tasks:vacuum"
-  command "echo 'Finish run rake vacuum at #{Time.now.to_s}' >> output.txt"
-end
-
-every 10.minutes, RAILS_ENV: :development do
-  command "sidekiq -c 1"
-  command "echo 'Finish run sidekiq -c 1 #{Time.now.to_s}' >> output.txt"
+every 1.hours, RAILS_ENV: :development do
+  rake "vacuum"
 end
