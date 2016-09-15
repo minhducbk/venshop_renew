8f5b66684ffd4d82cd7ca061b0c4e8042da274d0
Minh Duc Bui <ducbk95@gmail.com>
2016-09-15 17:09:20 +0700

    Fix asset bug in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile =  ['*.js', '*.jpg', '*.png', '*.scss', '*.scss', '*.coffee']
-  config.serve_static_assets = false
+  # config.assets.precompile =  ['*.js', '*.jpg', '*.png', '*.scss', '*.scss', '*.coffee']
+  # config.serve_static_assets = false
   # config.assets.digest = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
